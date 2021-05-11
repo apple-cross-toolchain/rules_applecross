3ab915ebf136bcef336cb3702df18f4f0d1b1df4
Thi Doãn <t@thi.im>
2021-05-11 16:18:24 +0900

    Create /usr/libexec before installing PlistBuddy
    This directory does not exist on Linux.

--- tools/install-mandatory-tools.sh
+++ tools/install-mandatory-tools.sh
@@ -12,5 +12,6 @@ pushd "${TMP_DIR}"
 curl -# -LO https://github.com/apple-cross-toolchain/ci/releases/download/0.0.4/ported-tools-linux-x86_64.tar.xz
 tar -xf ported-tools-linux-x86_64.tar.xz
 install bin/codesign bin/plutil bin/sw_vers bin/xcrun bin/xcodebuild bin/xcode-select /usr/bin/
+mkdir -p /usr/libexec
 install bin/PlistBuddy /usr/libexec/
 popd
